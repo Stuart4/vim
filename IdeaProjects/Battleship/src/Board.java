15d3c3bd38cb0fde82dc9fb3e59b9e126dfa695b
Jacob Stuart <stuart4@purdue.edu>
2014-07-04 20:41:03 -0700

    Randomly places boats
    ArrayIndexOutOfBoundsException somewhere

--- IdeaProjects/Battleship/src/Board.java
+++ IdeaProjects/Battleship/src/Board.java
@@ -10,14 +10,62 @@ public class Board {
 	Board (){
 		Random rnd = new Random();
 //		System.out.println(rnd.nextInt(8));
+
+		Ship[] ships = new Ship[2];
+		ships[0] = new AttackBoat();
+		ships[1] = new Carrier();
+
+		for (Ship k : ships){
+			 while (!placeRnd(k));
+		}
+
 		System.out.println(this.toString());
 	}
 
+	private boolean placeRnd(Ship ship){
+		Random rnd = new Random();
+
+		if (rnd.nextBoolean()) {
+			int index = rnd.nextInt(8);
+			int anInt = rnd.nextInt(8);
+			if (doesFit(ship, index, board[anInt])) {
+				placeShip(ship, true, index, anInt);
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			int index = rnd.nextInt(8);
+			int columnIndex = rnd.nextInt(8);
+			if (doesStand(ship, index, columnIndex)){
+				placeShip(ship, false, index, columnIndex);
+				return true;
+			} else {
+				return false;
+			}
+			}
+	}
+
+	private void placeShip(Ship ship, boolean horizontal, int startX, int startY){
+		if (horizontal){
+			for (int i = 0; i < ship.size; i++){
+				board[startY][startX + i] = 1;
+			}
+		} else {
+			for (int i = 0; i < ship.size; i++){
+				board[startY + i][startX] = 1;
+			}
+		}
+	}
 	private boolean doesFit(Ship ship, int index, int[] spaces){
 
 		int length = ship.getSize();
 
-		for (int i = index; i < spaces.length && i < length; i++){
+		if ( (length + index) > spaces.length - 1){
+			return false;
+		}
+
+		for (int i = index; i < spaces.length && i < length + index; i++){
 			if (spaces[i] == 1){
 				return false;
 			}
@@ -37,7 +85,7 @@ public class Board {
 	}
 
 	public String toString(){
-		String out = "";
+		String out = " ";
 
 		for (int i = 0; i < board[0].length; i++){
 			out += " " + Integer.toString(i + 1) + " ";
@@ -50,6 +98,8 @@ public class Board {
 			for (int n = 0; n < board.length; n++){
 				switch (board[i][n]){
 					case (0):
+						out += " % ";
+						break;
 					case (1):
 						out += " = ";
 						break;
